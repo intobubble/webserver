e85e9d92177f6cc88024af248667a6e6094afecb
intobubble <qyd4v8id@gmail.com>
2025-02-24 21:38:57 +0900

    fix: middleware

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 use axum::routing::{get, put};
 use axum::Router;
 use config::AppConfig;
-use serde::Serialize;
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
-use tracing::Span;
+
 pub mod config;
 pub mod handlers;
-
-#[derive(Serialize, Debug)]
-struct RequestLog {
-    path: String,
-    method: String,
-}
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().unwrap();
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .init();
 
     let app_conf = AppConfig::from_env();
@@ -38,16 +31,6 @@ fn app() -> Router {
         .route("/object", put(handlers::bucket::put_object::handle))
         .route("/object", get(handlers::bucket::get_object::handle))
         .route("/object/list", get(handlers::bucket::list_objects::handle))
-        .layer(
-            ServiceBuilder::new().layer(TraceLayer::new_for_http().on_request(
-                |req: &axum::http::Request<_>, _span: &Span| {
-                    let l = RequestLog {
-                        path: req.uri().path().to_string(),
-                        method: req.method().to_string(),
-                    };
-                    tracing::info!("{}", serde_json::to_string(&l).unwrap())
-                },
-            )),
-        )
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
         .with_state(app_conf)
 }
